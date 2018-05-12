ea79996b327cac019ad8019e87cc1829ec3aff20
unknown <jim88star@gmail.com>
2018-05-11 22:39:38 -0500

    original files

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+
+
+
+
+$(document).ready(function () {
+  $(".button-collapse").sidenav();
+});
+
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
+
+$(document).ready(function () {
+  $('.parallax').parallax();
+});
+
+$(document).ready(function () {
+  $('.carousel').carousel();
+
+  setInterval(function () {
+    $('.carousel').carousel('next');
+  }, 2000);
+});
+
+$(document).ready(function () {
+  $('.modal').modal();
+});
+
